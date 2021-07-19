dfe715f2114f1c9d1ee9803dcd6bfef1a427e803
Edison Velasco Sánchez <35384396+EPVelasco@users.noreply.github.com>
2021-07-19 15:29:58 +0200

    Add files via upload

--- test.m
+++ test.m
@@ -1,9 +1,41 @@
 close all;clear;clc
-scene=figure;  
+figure; 
+subplot(3,2,1)
 axis equal; 
 grid on; 
 xlabel('x (m)')
 ylabel('y (m)')
 zlabel('z (m)')
-camlight('headlight');
-bluePlot(0,0,0,0,0,000.1)
+bluePlot(0,0,0,0,0.4,0.001)
+
+subplot(3,2,3)
+view(90,0)
+axis equal; 
+grid on; 
+xlabel('x (m)')
+ylabel('y (m)')
+zlabel('z (m)')
+camlight('rigth');
+bluePlot(0,0,0,0,0.4,0.001)
+
+subplot(3,2,5)
+view(90,0)
+axis equal; 
+grid on; 
+xlabel('x (m)')
+ylabel('y (m)')
+zlabel('z (m)')
+camlight('rigth');
+bluePlot(0,0,0,pi/2,0.4,0.001)
+
+subplot(1,2,2)
+view(45,45)
+axis equal; 
+grid on; 
+xlabel('x (m)')
+ylabel('y (m)')
+zlabel('z (m)')
+camlight('HEADLIGHT');
+bluePlot(0,0,0,0,0.4,0.001)
+
+
